6815fa1d08a8a30ab25a7d9504d8daac4df69531
Steve Meredith <Steveeeie@users.noreply.github.com>
2025-05-12 02:02:56 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,9 @@ type ClassNamesArg = string | 0 | false | null | undefined;
 
 /**
  * A utility function that merges multiple class names into a single space-separated string.
- *
- * Filters out falsy values including: `false`, `null`, `undefined`, numeric zero (`0`).
- *
+ * Filters out falsy values including: `false`, `null`, `undefined` and `0`.
  * @param args - A list of class name values to be merged.
  * @returns A single string containing valid class names separated by spaces.
- *
  * @example classNames(styles.button, active && styles.active, styles[size]);
  */
 function classNames(...args: ClassNamesArg[]) {
